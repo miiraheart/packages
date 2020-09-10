a00bf2bbdc78fe4a785a8f6fefff8c9eb3aefd35
Your Name <you@example.com>
2020-09-10 13:18:58 +0400

    addedd ocp packages

---
+++ variables.sh
@@ -0,0 +1,6 @@
+export OCP_RELEASE=4.5.6-x86_64
+export LOCAL_REGISTRY='rtauevtrsmgtst1.ocptrsstg01.rtatestdom.local:5000' 
+export LOCAL_REPOSITORY='ocp4/openshift4' 
+export PRODUCT_REPO='openshift-release-dev' 
+export LOCAL_SECRET_JSON='/root/ocp-inst/pull-secret.json' 
+export RELEASE_NAME="ocp-release"
